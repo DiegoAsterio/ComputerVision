34e9d8b23e958a7decc2896da190813c32c237d9
DiegoAsterio <diegoasterio.z@gmail.com>
2018-10-08 13:02:27 +0200

    Changing structure

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import core
+import numpy as np
+import cv2 as cv
+
+if __name__=="__main__":
+    im = core.leeImagen("../test/hm.jpg", cv.IMREAD_COLOR)
+    im2 = core.calculateGaussian(im , 7, 7)
+    im3 = core.calculateGaussian(im, 9, 20)
+    vim = np.array([im, im2, im3])
+    core.pintaVarias(vim)
+    
